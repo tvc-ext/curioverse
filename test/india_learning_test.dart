69847664a8e3c2653b32fc53d15f6455f26dd4ac
tvc-ext <tvc.ext@gmail.com>
2026-08-15 15:53:26 +0530

    Test new visual discovery packs

--- test/india_learning_test.dart
+++ test/india_learning_test.dart
@@ -19,4 +19,16 @@ void main() {
     expect(history.storyPages, hasLength(3));
     expect(history.questions, hasLength(3));
   });
+
+  test('wonders, evolution and animal packs include stories and quizzes', () {
+    for (final id in [
+      'seven_wonders',
+      'human_evolution',
+      'animal_detective',
+    ]) {
+      final topic = learningTopics.singleWhere((candidate) => candidate.id == id);
+      expect(topic.storyPages, hasLength(3));
+      expect(topic.questions, hasLength(3));
+    }
+  });
 }
