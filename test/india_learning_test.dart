d7901fef41f7e2eff0c8a4c9d9f6777c89cd53fe
tvc-ext <tvc.ext@gmail.com>
2026-08-15 15:48:57 +0530

    Test ISRO and India learning packs

---
+++ test/india_learning_test.dart
@@ -0,0 +1,22 @@
+import 'package:curioverse/models/child_profile.dart';
+import 'package:curioverse/models/learning_topic.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('ISRO and Indian history adventures serve every age band', () {
+    final isro =
+        learningTopics.singleWhere((topic) => topic.id == 'isro_mission_lab');
+    final history =
+        learningTopics.singleWhere((topic) => topic.id == 'india_time_travel');
+
+    for (final ageBand in AgeBand.values) {
+      expect(isro.supports(ageBand), isTrue);
+      expect(history.supports(ageBand), isTrue);
+    }
+
+    expect(isro.storyPages, hasLength(3));
+    expect(isro.questions, hasLength(3));
+    expect(history.storyPages, hasLength(3));
+    expect(history.questions, hasLength(3));
+  });
+}
